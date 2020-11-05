4baee631e91859e620cbb847ca9e136b8ba0673e
cSDes1gn <christian911@sympatico.ca>
2020-11-05 12:36:17 -0500

    demonstrate library use

--- src/src.ino
+++ src/src.ino
@@ -4,7 +4,7 @@
  * 
  * @file src.ino
  *
- * @brief Sample source file
+ * @brief Sample source file entry point demonstrating local include calls.
  *
  * @author <Name>
  * Contact: <Email>
@@ -15,11 +15,16 @@
 #include <Lib1.h>
 #include <Lib2.h>
 
+Lib1 lb1("Hello from Lib1!");
+Lib2 lb2("Hello from Lib2!");
+
 void setup() {
     Serial.begin(9600);
 }
 
 void loop() {
-    Serial.println("Hello World!")
+    Serial.println("Hello World!");
+    Serial.println(lb1.echo());
+    Serial.println(lb2.echo());
     delay(1000);
 }
