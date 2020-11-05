15a51925d12ac40f518703c74371b3af87dbbc14
cSDes1gn <christian911@sympatico.ca>
2020-11-05 11:39:04 -0500

    top level package

---
+++ Package.h
@@ -0,0 +1,11 @@
+// -*- lsst-c++ -*-
+/**
+ * @defgroup PackageName
+ *
+ * @brief Template repository for arduino projects
+ * 
+ * Copyright [year] <name>.
+ */
+#ifndef PACKAGE_H_
+#define PACKAGE_H_
+#endif  // PACKAGE_H_
